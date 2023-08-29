e38467ac99fd7c78a5ab5baa69db69b7f3ef66d0
RunThem <iccy.fun@outlook.com>
2023-08-29 20:18:01 +0800

    update demo file

--- src/hello.c
+++ src/hello.c
@@ -1,19 +1,40 @@
 
-void boo() {
-}
 
-void foo() {
-  boo();
-}
+enum en {
+  S_INIT,
+  S_START,
+  S_END,
+};
+
+struct st {
+  int nems;
+  char* msg;
+};
 
 typedef struct {
   int a;
   char c;
 } st_t;
 
+void boo() {
+  enum en e = S_INIT;
+}
+
+void foo() {
+  boo();
+}
+
 int main(int argc, const char** argv) {
   int a   = 0;
   st_t st = {0};
+
+  struct {
+    int d;
+    union {
+      int l;
+      int* ptr;
+    };
+  } anonymity_st = {0};
 
   foo();
 
